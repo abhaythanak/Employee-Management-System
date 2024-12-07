c11eb7bb83601d20960422e34ccd8aafce4e7063
abhaythanak <abhaythanak@gmail.com>
2024-12-08 00:13:43 +0530

    employrr management system set up 1.required file and folder created Auth Login section UI created LOgical section isn't done yet

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        console.log("email", email)
+        console.log("password", password)
+        setEmail("")
+        setPassword("")
+    }
+  return (
+    <div className='flex h-screen w-screen justify-center items-center '>
+        <div className="border-2 rounded-lg border-emerald-500 p-20">
+            <form 
+            onSubmit={(e) => submitHandler(e)} 
+            className="flex flex-col items-center justify-center gap-3">
+                <input
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+                required 
+                className='border-2 py-3 px-5 rounded-full border-emerald-500 text-xl  outline-none bg-transparent placeholder:text-gray-100' type="email" placeholder='Enter your email'/>
+                <input
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                required 
+                className='border-2 py-3 px-5 rounded-full border-emerald-500 text-xl  outline-none bg-transparent placeholder:text-gray-100'  type="password" placeholder='Enter your password'/>
+                <button className=' py-3 px-5 rounded-full bg-emerald-500 text-xl text-white outline-none bg-transparent '>Submit</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
